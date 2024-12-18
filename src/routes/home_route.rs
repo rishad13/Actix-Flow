667af97d6863078aa76a7309b71b57d4b9a2526e
rishadn <rishad.n@codinghands.in>
2024-12-18 17:46:36 +0530

    fix

--- src/routes/home_route.rs
+++ src/routes/home_route.rs
@@ -1,24 +1,16 @@
 use super::handlers;
 use actix_web::web;
 
+/// Configure the actix-web route services for the home page.
 ///
-/// Registers routes for the "home" scope.
+/// Registers the route services for the home page at the `/home` path.
+/// The services registered include the `index` and `test` handlers.
 ///
-/// The "home" scope includes the following routes:
-///
-/// - `GET /home`: Returns a message with a Rust mascot.
-/// - `GET /`: Returns a message with a Rust mascot.
-/// - `GET /test`: Returns a message with a Rust mascot.
-///
-/// # Parameters
-///
-/// * `config`: An instance of `actix_web::web::ServiceConfig` to configure.
-///
-/// # Returns
-///
-/// * None
 pub fn config(config: &mut web::ServiceConfig) {
-    config.service(web::scope("/home").service(handlers::home_handler::index))
-        .service(handlers::home_handler::index)
-        .service(handlers::home_handler::test);
+    config.service(
+        web::scope("/home")
+            .service(handlers::home_handler::index)
+            .service(handlers::home_handler::index)
+            .service(handlers::home_handler::test),
+    );
 }
